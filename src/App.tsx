79318cb986f9b30039d4479e9a14039c013ee259
Carstea-Sebastian <csimlu73@gmail.com>
2025-11-14 13:46:02 +0200

    Stilizare + implementare afisare si eliminare task

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,10 @@ import { ITask } from './Interfaces';
 import TodoTask from './Components/TodoTask';
 
 const App:FC=() =>{
-
   const [task, setTask]=useState<string>("");
   const [deadline, setDeadline]=useState<number>(0);
   const [todoList, setTodoList]=useState<ITask[]>([]);
-
+  
   const handleChange = (event:ChangeEvent<HTMLInputElement>): void =>{
    if(event.target.name==="task"){
         setTask(event.target.value)
@@ -16,34 +15,40 @@ const App:FC=() =>{
     setDeadline(Number(event.target.value));
    }
   }
-
+  
   const addTask=():void=>{
     const newTask={taskName:task,deadline:deadline};
     setTodoList([...todoList,newTask]);
     setTask("");
     setDeadline(0);
   }
-
+  
+  const completeTask = (taskNameToDelete:string):void=>{
+    setTodoList(todoList.filter((task)=>{
+      return task.taskName !== taskNameToDelete;
+    }))
+  }
+  
   return (
-    <div className="App">
+    <div className='App'>
       <div className="header">
         <div className="inputContainer"> 
           <input 
           type="text" 
-          placeholder='Task...' 
-          name="task" 
+          placeholder="Task..."
+          name='task' 
           value={task}
           onChange={handleChange}/>
-
           <input type="number" 
           placeholder='Deadline' 
-          name="deadline" 
+          name="deadline"
+          value={deadline}
           onChange={handleChange}/>
           <button onClick={addTask}>Add Task</button>
         </div>
         <div className="todoList">
           {todoList.map((task: ITask,key:number)=>{
-            return<TodoTask key={key} task={task}/>;
+            return<TodoTask key={key} task={task} completeTask={completeTask}/>;
           })}
         </div>
       </div>
